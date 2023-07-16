bcf26982efcbd3d4759a7b934781826edceb1626
zena24-meet <siatbr.siatbr@gmail.com>
2023-07-16 18:57:39 +0300

    Update pythonreview.py

--- pythonreview.py
+++ pythonreview.py
@@ -1,1 +1,19 @@
-lhfjsflkjdfj
+def create_youtube_video(title,description):
+	youtubevideo= {"title":title,"description":description,"likes":0,"dislikes":0, "comments":{}}
+	return youtubevideo
+
+def like(youtubevideo):
+	youtubevideo["likes"] += 1
+	return youtubevideo
+
+def dislike(youtubevideo):
+	youtubevideo["dislikes"] += 1
+	return youtubevideo
+	
+
+def add_comments():
+	
+
+
+
+	
