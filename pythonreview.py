455fe728191000df91248926b112885566cb3f46
zena24-meet <siatbr.siatbr@gmail.com>
2023-07-17 18:23:12 +0300

    Update pythonreview.py

--- pythonreview.py
+++ pythonreview.py
@@ -11,7 +11,26 @@ def dislike(youtubevideo):
 	return youtubevideo
 	
 
-def add_comments():
+def add_comments(youtubevideo, username, comment_text):
+   if "comments"not in youtubevideo:
+   	youtubevideo["comments"] = {}
+   youtubevideo["comments"] [username] = comment_text
+   return youtubevideo
+
+youtubevideo = create_youtube_video("My Video", "Youtuber in the making :()")
+print(youtubevideo)
+
+youtubevideo = likr(youtubevideo)
+print(youtubevideo)
+
+youtubevideo = dislike(youtubevideo)
+print(youtubevideo)
+
+youtubevideo = add_comments(youtubevideo, "hello", "hi", "amayzing")
+print(youtubevideo)
+
+youtubevideo = int(youtubevideo["Likes"]) + 494
+print(youtubevideo)
 	
 
 
